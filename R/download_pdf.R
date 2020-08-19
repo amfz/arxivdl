56453c706018d5eeb438ceb27ff908918aad8ba9
AM <amany.mahfouz@gmail.com>
2020-08-18 20:10:07 -0400

    Add files via upload

--- R/download_pdf.R
+++ R/download_pdf.R
@@ -3,8 +3,9 @@
 #' Takes the results of an arxiv API query and downloads papers to disk by default. Saving to an AWS S3 bucket is also supported, though credentials must be set as environment variables prior to using `download_pdf` (see example).
 #' Papers are saved with their arXiv id, e.g. "2001.12345v1.pdf"
 #'
-#' @param data data frame of arXiv records
-#' @param link_col name of column containing the PDF locations. `link_pdf` by default.
+#' @param data data frame of arXiv records.
+#' @param links name of column containing the PDF locations. `link_pdf` by default.
+#' @param fnames name of column whose values should be used for saved file names. `id` by default.
 #' @param directory directory to download files to. Current working directory by default.
 #' @param bucket name of AWS S3 bucket to save files to.
 #'
@@ -22,15 +23,30 @@
 #' # download_pdf(ml, bucket="mybucket")
 #'
 #' @export
-download_pdf <- function(data, link_col = "link_pdf", directory = ".", bucket = NULL) {
+download_pdf <- function(data, links = "link_pdf", fnames = "id", directory = ".", bucket = NULL) {
   for (i in 1:nrow(data)) {
-    url <- paste0(data[i, link_col], ".pdf")
-    file_name <- paste0(data[i, "id"], ".pdf")
+
+    # make sure the link ends with ".pdf"
+    if (!endsWith(data[i, links], ".pdf")) {
+      url <- paste0(data[i, links], ".pdf")
+    } else {
+      url <- data[i, links]
+    }
+
+    # ditto the file name
+    if (!endsWith(data[i, fnames], ".pdf")) {
+      file_name <- paste0(data[i, fnames], ".pdf")
+    } else {
+      file_name <- data[i, fnames]
+    }
     print(file_name)
+
+    # if no bucket is specified, save to disk
     if (is.null(bucket)) {
       save_path <- file.path(directory, file_name)
       utils::download.file(url, save_path, mode = "wb")
     } else {
+      # check system environment for s3 credentials
       if (nchar(Sys.getenv("AWS_ACCESS_KEY_ID")) < 16) {
         warning("AWS_ACCESS_KEY_ID environment variable not set")
       }
@@ -40,6 +56,7 @@ download_pdf <- function(data, link_col = "link_pdf", directory = ".", bucket = 
       if(nchar(Sys.getenv("AWS_DEFAULT_REGION")) < 2) {
         warning("AWS_DEFAULT_REGION environment variable not set")
       }
+      # write to s3 bucket
       aws.s3::s3write_using(url,
                             FUN = utils::download.file,
                             mode = "wb",
---
+++ R/utility_functions.R
@@ -0,0 +1,26 @@
+#' Clean Title Names
+#'
+#' Standardize a text vector
+#'
+#' @param data data frame of arXiv records.
+#' @param col text column in the data frame to clean up
+#'
+#' @return Character vector of cleaned text.
+#'
+#' @examples
+#'
+#'
+#'
+#' @export
+clean_titles <- function(data, col = "title") {
+  # make the titles all lowercase
+  cleaned <- tolower(data[, col])
+  # replace punctuation with spaces
+  cleaned <- gsub("[[:punct:]]", " ", cleaned)
+  # reduce multiple whitespace chars to one
+  cleaned <- gsub("[[:space:]]{2,}", " ", cleaned)
+  # replace spaces with underscores
+  cleaned <- gsub("[[:space:]]", "_", cleaned)
+  cleaned <- paste0(cleaned, ".pdf")
+  cleaned
+}
